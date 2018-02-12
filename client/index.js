eecd31eab19e0509a5fc6bea0a62688b95268ef0
Robert Saunders <robertwsaunders@outlook.com>
2018-02-11 21:30:14 -0500

    adding editor config, eslint, webpack, github templates, circleci config and more

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => {
+  return (
+    <div>
+      Hello Hop!
+    </div>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
